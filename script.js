77d936f7b70eff7bd134c91be071ed3353908546
Santiago Barrett <84517048+Santiago-zavala-barrett@users.noreply.github.com>
2021-06-16 14:05:17 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var day = "Friday";
+var age = 12;
+if ((day == "Tuesday" || day =="Thursday") || (age > 12 && age < 21)) {
+  console.log("You're eligible for student pricing!");
+} else {
+  console.log("You must pay normal price.")
+}
