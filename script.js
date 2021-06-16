a62f5c0a1728ef2da6aa6c30defc85eefc448a3e
Santiago Barrett <84517048+Santiago-zavala-barrett@users.noreply.github.com>
2021-06-16 14:37:44 +0000

    fixing errors

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let day = 'Friday'
 let age = 12
-if ((day === 'Tuesday' || day ==='Thursday') || (age > 12 && age < 21)) {
+if ((day === 'Tuesday' || day === 'Thursday') || (age > 12 && age < 21)) {
   console.log("You're eligible for student pricing!")
 } else {
   console.log('You must pay normal price.')
